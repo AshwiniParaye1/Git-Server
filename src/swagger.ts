14b6bbe16aba15c83a7de11dabc71bad5eb4d9b4
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-03-09 14:15:48 +0530

    update server url

--- src/swagger.ts
+++ src/swagger.ts
@@ -10,7 +10,7 @@ const options: swaggerJsdoc.Options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: "https://git-server-lo5u.onrender.com/",
         description: "Development server"
       }
     ],
